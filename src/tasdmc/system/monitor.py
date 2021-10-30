99b33712119bf926f189889421fc4a6e474fa5cb
Igor Vaiman <gosha.vaiman@gmail.com>
2021-10-30 16:34:13 +0300

    rough error handling in system monitor

--- src/tasdmc/system/monitor.py
+++ src/tasdmc/system/monitor.py
@@ -13,23 +13,25 @@ def run_system_monitor():
     interval = float(config.get_key("resources.monitor_interval", default=60))
     if interval <= 0:
         return
+
     while True:
         time_before_measurement = time.time()
 
         processes = get_run_processes(fileio.get_saved_main_pid())
-        if processes is None:
-            return  # run is dead, exiting monitor
-        running_processes = [p for p in processes if p.status() == psutil.STATUS_RUNNING and p.pid != os.getpid()]
-
-        if running_processes:
-            cpu_percents = [p.cpu_percent(0.3) for p in running_processes]
-            #                    same as top‘s RES column
-            memory_used = [bytes2Gb(p.memory_info().rss) for p in running_processes]
-            logs.system_resources_info(
-                "CPU " + " ".join(f"{cp:.3f}" for cp in cpu_percents)
-                + " MEM " + " ".join(f"{m:.3f}" for m in memory_used)
-                + f" DISK {directory_size(fileio.run_dir()):.3f}/{available_disk_space(fileio.run_dir()):.3f}"
-            )
+        if processes is not None:
+            try:
+                running_processes = [p for p in processes if p.status() == psutil.STATUS_RUNNING and p.pid != os.getpid()]
+                if running_processes:
+                    cpu_percents = [p.cpu_percent(0.3) for p in running_processes]
+                    #                    same as top‘s RES column
+                    memory_used = [bytes2Gb(p.memory_info().rss) for p in running_processes]
+                    logs.system_resources_info(
+                        "CPU " + " ".join(f"{cp:.3f}" for cp in cpu_percents)
+                        + " MEM " + " ".join(f"{m:.3f}" for m in memory_used)
+                        + f" DISK {directory_size(fileio.run_dir()):.3f}/{available_disk_space(fileio.run_dir()):.3f}"
+                    )
+            except Exception:
+                pass
 
         time_after_measurement = time.time()
         time.sleep(max(1, interval - (time_after_measurement - time_before_measurement)))
