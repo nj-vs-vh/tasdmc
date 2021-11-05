5a18e311adeea6f93dde95d30013d3a280ad595d
Igor Vaiman <gosha.vaiman@gmail.com>
2021-11-05 19:33:27 +0300

    autocomplete script bash version check fixed

--- conda-build-recipe/scripts/tasdmc-autocomplete.sh
+++ conda-build-recipe/scripts/tasdmc-autocomplete.sh
@@ -2,7 +2,7 @@
 
 tasdmc >/dev/null 2>/dev/null  # testing if tasdmc is actually runable
 
-if [ $? -eq 0 ] && [ "$BASH_VERSION" > "4.4" ]
+if [ $? -eq 0 ] && [[ "$BASH_VERSION" > "4.4" ]]
 then
     eval "$(_TASDMC_COMPLETE=bash_source tasdmc)"
 fi
