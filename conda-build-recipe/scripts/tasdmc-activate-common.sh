4ccc30932fb3965669cd8f7e2bb66dedc24c960d
Igor Vaiman <gosha.vaiman@gmail.com>
2021-11-06 17:32:54 +0300

    conda stuff removed, saved at a dedicated branch

--- conda-build-recipe/scripts/tasdmc-activate-common.sh
+++
@@ -1,6 +0,0 @@
-# activation script, executed on user system during env activation
-
-# in conda package tasdmc C routines are installed into env's bin
-export TASDMC_BIN_DIR=$CONDA_PREFIX/bin
-# in conda package tasdmc is build with constant 2 Gb per process memory allocation, see build.sh
-export TASDMC_MEMORY_PER_PROCESS_GB=2
--- conda-build-recipe/scripts/tasdmc-autocomplete.sh
+++
@@ -1,8 +0,0 @@
-# activation script, executed on user system during env activation
-
-tasdmc >/dev/null 2>/dev/null  # testing if tasdmc is actually runable
-
-if [ $? -eq 0 ] && [[ "$BASH_VERSION" > "4.4" ]]
-then
-    eval "$(_TASDMC_COMPLETE=bash_source tasdmc)"
-fi
