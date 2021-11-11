70db2deccc0abc52a00ed5638d4c5330b85cd39e
Igor Vaiman <gosha.vaiman@gmail.com>
2021-11-11 23:18:42 +0300

    test env configured

---
+++ tests/test_env.sh
@@ -0,0 +1,7 @@
+# testing environment
+# these values are not actually used for now, so just exporting sentinel values
+
+export TASDMC_RUNS_DIR="/"
+export TASDMC_DATA_DIR="/"
+export TASDMC_BIN_DIR="/"
+export TASDMC_MEMORY_PER_PROCESS_GB="2"
