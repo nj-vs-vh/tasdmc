ce62868465c726f7f2c81b40884fe6fa00cb4008
Igor Vaiman <gosha.vaiman@gmail.com>
2021-12-02 17:45:01 +0300

    dry option added to distr run

--- src/tasdmc/cli/commands.py
+++ src/tasdmc/cli/commands.py
@@ -44,14 +44,22 @@ def local_run_cmd(run_config_filename: str):
 
 
 @cli.command("run-distributed", help="Run simulation distributed across several machines (nodes)")
+@click.option(
+    "--dry",
+    is_flag=True,
+    default=False,
+    help="Check that all nodes are connectable and capable of running the simulation",
+)
 @run_config_option('run_config_filename')
 @nodes_config_option('nodes_config_filename')
 @error_catching
-def distributed_run_cmd(run_config_filename: str, nodes_config_filename: str):
+def distributed_run_cmd(run_config_filename: str, nodes_config_filename: str, dry: bool):
     config.RunConfig.load(run_config_filename)
     config.NodesConfig.load(nodes_config_filename)
     nodes.check_all()
     nodes.run_all_dry()
+    if dry:
+        return
     fileio.prepare_run_dir()
     nodes.run_all()
 
