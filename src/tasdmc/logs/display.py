57c33d15cfb1f289526d6dd53c23defb2c86e7ea
Igor Vaiman <gosha.vaiman@gmail.com>
2021-12-07 19:38:19 +0300

    marker updated for abs t system resources plot

--- src/tasdmc/logs/display.py
+++ src/tasdmc/logs/display.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass, asdict
 from math import ceil
 import json
 from pathlib import Path
+from functools import partial
 
 from typing import List, Optional
 
@@ -232,7 +233,7 @@ class SystemResourcesTimeline:
             xs = [dt.timestamp() for dt in self.timestamps]
             tick_label_from_value = lambda x: datetime2str(datetime.fromtimestamp(x))
             x_axis_label = "Datetime, UTC"
-            plot_fn = plt.scatter
+            plot_fn = partial(plt.scatter, marker='x')
         else:
             xs = [td.total_seconds() for td in self.ret]
             tick_label_from_value = lambda x: timedelta2str(timedelta(seconds=x))
@@ -285,140 +286,3 @@ class SystemResourcesTimeline:
         plt.xticks(x_ticks, x_tick_labels)
 
         plt.show(allow_scrolling=True)
-
-
-def print_system_monitoring(include_previous_runs: bool = False, evaluation_time_as_x: bool = True):
-    timestamps: List[datetime] = []
-    cpu: List[float] = []
-    mem: List[float] = []
-    disk_used: List[float] = []
-    disk_avl: List[float] = []
-
-    logs_dirs_to_look_in = [fileio.logs_dir()]
-    if include_previous_runs:
-        logs_dirs_to_look_in.extend(fileio.get_previous_logs_dirs())
-    system_resources_log_filename = fileio.system_resources_log().name
-
-    entry_re = re.compile(
-        r"^\[(?P<timestamp>.*?)\] CPU (?P<cpu_perc>.*?) MEM (?P<mem_usage>.*?) "
-        + r"DISK (?P<disk_used_by_run>.*?)/(?P<disk_available>.*?)$"
-    )
-
-    run_start_indices: List[int] = [0]
-    for logs_dir in logs_dirs_to_look_in:
-        system_resources_log = logs_dir / system_resources_log_filename
-        if not system_resources_log.exists():
-            continue
-        n_entries_before = len(timestamps)
-        for entry in system_resources_log.read_text().splitlines():
-            m = entry_re.match(entry)
-            try:
-                entry_timestamp = str2datetime(m.group('timestamp'))
-                cpu_entry = sum(
-                    [float(cpu_per_worker) for cpu_per_worker in m.group('cpu_perc').split() if cpu_per_worker]
-                )
-                mem_entry = sum(
-                    [float(mem_per_worker) for mem_per_worker in m.group('mem_usage').split() if mem_per_worker]
-                )
-                disk_used_entry = float(m.group('disk_used_by_run'))
-                disk_avl_entry = float(m.group('disk_available'))
-
-                timestamps.append(entry_timestamp)
-                cpu.append(cpu_entry)
-                mem.append(mem_entry)
-                disk_used.append(disk_used_entry)
-                disk_avl.append(disk_avl_entry)
-            except Exception:
-                click.secho(f"Can't parse system resources log entry: '{entry}'")
-        n_entries_after = len(timestamps)
-        if n_entries_after > n_entries_before:
-            run_start_indices.append(n_entries_after)  # next run will/would start here
-    if not timestamps:
-        click.echo("No recorded system resources found for the run")
-        return
-
-    run_start_indices = run_start_indices[:-1]
-
-    if include_previous_runs:
-        cpu = [t[1] for t in sorted(zip(timestamps, cpu))]
-        mem = [t[1] for t in sorted(zip(timestamps, mem))]
-        disk_used = [t[1] for t in sorted(zip(timestamps, disk_used))]
-        disk_avl = [t[1] for t in sorted(zip(timestamps, disk_avl))]
-        run_start_timestamps = sorted([timestamps[i] for i in run_start_indices])
-        timestamps.sort()
-        run_start_indices = [timestamps.index(rst) for rst in run_start_timestamps]
-
-    click.echo(f"System resources (as last monitored at {datetime2str(timestamps[-1])}):")
-    click.echo(f"Total CPU utilization (100% is 1 fully utilized core): {cpu[-1]:.2f}%")
-    click.echo(f"Total memory consumed: {mem[-1]:.2f} Gb")
-    click.echo(f"Disk used: {disk_used[-1]:.2f} Gb")
-    if len(timestamps) < 5:
-        click.echo("Plots are not available for less than 5 recorded data points")
-        return
-
-    if evaluation_time_as_x:
-        # converting timestamps (absolute times) to evaluation time as if all runs run without
-        # pauses between them; x axis then represents "run evaluation time" (RET)
-        run_eval_times: List[timedelta] = timestamps.copy()
-        previous_runs_evaluation_time = timedelta(seconds=0)
-        for run_start_i, run_end_i in zip(run_start_indices, [*run_start_indices[1:], len(run_eval_times)]):
-            run_start_dt = run_eval_times[run_start_i]
-            for i in range(run_start_i, run_end_i):
-                run_eval_times[i] = (run_eval_times[i] - run_start_dt) + previous_runs_evaluation_time
-            previous_runs_evaluation_time = run_eval_times[i] + timedelta(
-                minutes=1
-            )  # as if there was 1 min between runs
-        xs = [t.total_seconds() for t in run_eval_times]
-        tick_label_from_value = lambda x: timedelta2str(timedelta(seconds=x))
-        x_axis_label = "Run evaluation time"
-    else:
-        xs = [dt.timestamp() for dt in timestamps]
-        tick_label_from_value = lambda x: datetime2str(datetime.fromtimestamp(x))
-        x_axis_label = "Datetime, UTC"
-
-    x_n = len(xs)
-    x_tick_indices = [0, int(0.33 * x_n), int(0.66 * x_n), x_n - 1]
-    x_ticks = [xs[idx] for idx in x_tick_indices]
-    x_tick_labels = [tick_label_from_value(xs[idx]) for idx in x_tick_indices]
-
-    terminal_width, terminal_height = shutil.get_terminal_size()
-    plot_width, plot_height = min(100, terminal_width), min(30, terminal_height)
-
-    plt.clear_figure()
-    plt.subplots(3, 1)
-
-    # disk usage plot
-    plt.subplot(1, 1)
-    # plotting available disk space only whan we're running out of it (< 3 Gb left)
-    disk_avl_plot_data = [(t, da + du) for t, da, du in zip(xs, disk_avl, disk_used) if da < 3]
-    plt.plot(xs, disk_used, color='blue', label="Run directory" if disk_avl_plot_data else "")
-    if disk_avl_plot_data:
-        plt.plot(
-            [td[0] for td in disk_avl_plot_data],
-            [td[1] for td in disk_avl_plot_data],
-            color='red',
-            label="Max for run directory",
-        )
-    plt.title("Disk usage")
-    plt.ylabel("Disk usage, Gb")
-    plt.xlabel(x_axis_label)
-    plt.plotsize(plot_width, plot_height)
-    plt.xticks(x_ticks, x_tick_labels)
-
-    plt.subplot(2, 1)
-    plt.plot(xs, cpu, color='green')
-    plt.title("CPU utilization")
-    plt.ylabel("CPU, 100% per core")
-    plt.xlabel(x_axis_label)
-    plt.plotsize(plot_width, plot_height)
-    plt.xticks(x_ticks, x_tick_labels)
-
-    plt.subplot(3, 1)
-    plt.plot(xs, mem, color='teal')
-    plt.title("Memory usage")
-    plt.ylabel("RAM used, Gb")
-    plt.xlabel(x_axis_label)
-    plt.plotsize(plot_width, plot_height)
-    plt.xticks(x_ticks, x_tick_labels)
-
-    plt.show(allow_scrolling=True)
