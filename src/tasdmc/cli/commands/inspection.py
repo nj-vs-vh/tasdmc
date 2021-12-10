40fcc95ad73916cccbf0fc3c3375474c4c597eb5
Igor Vaiman <gosha.vaiman@gmail.com>
2021-12-10 13:11:44 +0300

    inspect/fix commands marked as unavailable for distributed run

--- src/tasdmc/cli/commands/inspection.py
+++ src/tasdmc/cli/commands/inspection.py
@@ -1,10 +1,9 @@
 import click
 
-from tasdmc import fileio, inspect, hard_cleanup
+from tasdmc import fileio, inspect, hard_cleanup, config
 
 from ..group import cli
-from ..options import run_config_option, nodes_config_option
-from ..utils import run_standard_pipeline_in_background, loading_run_by_name, error_catching
+from ..utils import loading_run_by_name, error_catching
 
 
 @cli.command("fix-failed", help="Fix failed pipelines")
@@ -12,6 +11,9 @@ from ..utils import run_standard_pipeline_in_background, loading_run_by_name, er
 @loading_run_by_name
 @error_catching
 def fix_failed_pipelines_cmd(hard: bool):
+    if config.is_distributed_run():
+        click.echo("Not available for distributed run, please fix your nodes manually")
+        return
     failed_pipeline_ids = fileio.get_failed_pipeline_ids()
     if not failed_pipeline_ids:
         click.echo("No failed pipelines to fix")
@@ -24,10 +26,13 @@ def fix_failed_pipelines_cmd(hard: bool):
 
 @cli.command("inspect", help="Inspect pipelines step-by-step")
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Print verbose information about steps")
-@click.option("-p", "--page", "pagesize", default=0, help="Page size or 0 for no pagination (default)")
+@click.option("-p", "--page", "pagesize", default=0, help="Page size; 0 for no pagination (default)")
 @click.option("-f", "--failed", is_flag=True, default=False, help="Inspect only failed pipelines")
 @loading_run_by_name
 @error_catching
 def inspect_cmd(pagesize: int, verbose: bool, failed: bool):
+    if config.is_distributed_run():
+        click.echo("Not available for distributed run, please inspect your nodes manually")
+        return
     pipeline_ids = fileio.get_failed_pipeline_ids() if failed else fileio.get_all_pipeline_ids()
     inspect.inspect_pipelines(pipeline_ids, page_size=pagesize, verbose=verbose, fix=False)
