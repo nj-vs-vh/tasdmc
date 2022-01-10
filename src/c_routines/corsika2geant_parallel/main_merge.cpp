36779a26f8628c51257b5a27d3e09c4e2d122bc5
Igor Vaiman <gosha.vaiman@gmail.com>
2022-01-10 23:38:45 +0300

    interpolation added

--- src/c_routines/corsika2geant_parallel/main_merge.cpp
+++ src/c_routines/corsika2geant_parallel/main_merge.cpp
@@ -3,70 +3,90 @@
 #include <string.h>
 #include <errno.h>
 #include <map>
+#include <algorithm>
+#include <math.h>
 
 #include "./structs.h"
+#include "./utils.h"
 #include "./constants.h"
 
 // {[x, y, t]: value}
 typedef std::map<std::array<short, 3>, unsigned short> Sparse3DMatrix;
 #define MAX_SPARSE3DMATRIX_SIZE ((double)NX * (double)NY * (double)TMAX) // ~10^10
 #define sparse3DMatrixLoadFactor(matrix) (double)(matrix.size()) / MAX_SPARSE3DMATRIX_SIZE
+#define getValueOrZero(matrix, key, assign_to) \
+    if (1)                                     \
+    {                                          \
+        auto it = matrix.find(key);            \
+        if (it == matrix.end())                \
+        {                                      \
+            assign_to = 0;                     \
+        }                                      \
+        assign_to = it->second;                \
+    }
 
 Sparse3DMatrix vem_top;
 Sparse3DMatrix vem_bot;
 Sparse3DMatrix pz;
 
+// all the time indices (k) in merged sparse array are stored relative to the this times and may be negative
 float reference_min_arrival_times[NX][NY];
+// min of min arrival times in each partial tile file
 float global_min_arrival_times[NX][NY];
 float current_min_arrival_times[NX][NY];
 
-bool readMinArrivalTimes(FILE *f, float arr[NX][NY])
+void initMinArrivalTimes(float arr[NX][NY])
 {
-    // filling arr with sentinel values
     for (short m = 0; m < NX; m++)
         for (short n = 0; n < NY; n++)
             arr[m][n] = SENTINEL_TIME;
+}
 
+bool readCurrentMinArrivalTimes(FILE *f)
+{
+    initMinArrivalTimes(current_min_arrival_times);
     short m;
     short n;
     float time;
     while (1)
     {
-        size_t total_bytes_read = fread(&m, sizeof(short), 1, f) +
-                                  fread(&n, sizeof(short), 1, f) +
-                                  fread(&time, sizeof(float), 1, f);
-        if (total_bytes_read != 3) // reached the end of the file, something's not right
+        size_t values_read = fread(&m, sizeof(short), 1, f) +
+                             fread(&n, sizeof(short), 1, f) +
+                             fread(&time, sizeof(float), 1, f);
+        if (values_read != 3) // reached the end of the file, something's not right
             return false;
-        if (m == -1) // reached delimiter triplet, no more min arrival time values
+        if (m == -1) // reached delimiter triplet, read is done
             return true;
-        arr[m][n] = time;
+        current_min_arrival_times[m][n] = time;
+        // the first min arrival time for (m, n) across all partial tile files is considered reference
+        if (reference_min_arrival_times[m][n] == SENTINEL_TIME)
+            reference_min_arrival_times[m][n] = time;
+        if (time < global_min_arrival_times[m][n])
+            global_min_arrival_times[m][n] = time;
     }
 }
 
-bool loadPartialTileFile(char *filename, EventHeaderData *event_data, bool initial)
+bool loadPartialTileFile(char *filename, EventHeaderData *event_data)
 {
     fprintf(stdout, "Reading partial tile file %s\n", filename);
     FILE *fptile = fopen(filename, "r");
     if (fptile == NULL)
     {
-        fprintf(stderr, "Cannot open file %s: %s\n", filename, strerror(errno));
+        fprintf(stderr, "Cannot open %s: %s\n", filename, strerror(errno));
         return false;
     }
 
     if (!readEventHeaderData(event_data, fptile))
     {
-        fprintf(stderr, "Error reading event header from partial tile file %s\n", filename);
+        fprintf(stderr, "Error reading event header from %s\n", filename);
         return false;
     }
 
-    if (!readMinArrivalTimes(fptile, current_min_arrival_times))
+    if (!readCurrentMinArrivalTimes(fptile))
     {
-        fprintf(stderr, "Error reading min arrival times from partial tile file %s\n", filename);
+        fprintf(stderr, "Error reading min arrival times from %s\n", filename);
         return false;
     }
-
-    if (initial)
-        memcpy(reference_min_arrival_times, current_min_arrival_times, NX * NY * sizeof(float));
 
     unsigned short buf[TILE_FILE_BLOCK_SIZE];
     std::array<short, 3> mnk;
@@ -96,6 +116,7 @@ bool loadPartialTileFile(char *filename, EventHeaderData *event_data, bool initi
 bool dumpTileFile(char *filename, EventHeaderData *ed)
 {
     FILE *fout = fopen(filename, "w");
+    fprintf(stdout, "Writing merged tile data to %s\n", filename);
     if (fout == NULL)
     {
         fprintf(stderr, "Cannot open file %s: %s\n", filename, strerror(errno));
@@ -118,16 +139,121 @@ bool dumpTileFile(char *filename, EventHeaderData *ed)
         buf[1] = mnk[1];     // n
         buf[2] = it->second; // vem_top
         buf[3] = vem_bot[mnk];
+        if (buf[2] == 0 && buf[3] == 0)
+            continue;
         // transforming # bins from min arrival time (in [0; TMAX]) to # bins from tmin
         buf[4] = (unsigned short)((reference_min_arrival_times[mnk[0]][mnk[1]] + (float)(mnk[2]) * DT - ed->tmin) / DT);
         buf[5] = pz[mnk];
         if (fwrite(buf, sizeof(short), TILE_FILE_BLOCK_SIZE, fout) != TILE_FILE_BLOCK_SIZE)
         {
+            fclose(fout);
             return false;
         }
     }
     fclose(fout);
     return true;
+}
+
+float interp_radius; // m
+int interp_halfside; // tiles from NX / 2
+
+void inetrpolateMinArrivalTimes()
+{
+    int m_edge = NX / 2;
+    while (global_min_arrival_times[m_edge][NY / 2] == SENTINEL_TIME)
+        m_edge++;
+    interp_radius = tileIndex2Coord(m_edge) + 2.0;
+    interp_halfside = m_edge - NX / 2 + 5;
+    printf("Interpolating tiles inside the %g m circle\n", interp_radius);
+
+    for (int m = NX / 2 - interp_halfside; m < NX / 2 + interp_halfside; m++)
+        for (int n = NY / 2 - interp_halfside; n < NY / 2 + interp_halfside; n++)
+        {
+            float x = tileIndex2Coord(m);
+            float y = tileIndex2Coord(n);
+            if (hypotf(x, y) < interp_radius)
+            {
+                float radius_ratio = (interp_radius + 7.5) / hypotf(x, y);
+                // interpolated time is lerp between times at closest and farthest points on the ring
+                float x_on_ring = 100 * x * radius_ratio; // m -> cm
+                float y_on_ring = 100 * y * radius_ratio; // m -> cm
+                int m_close = coord2TileIndex(x_on_ring);
+                int n_close = coord2TileIndex(y_on_ring);
+                int m_far = coord2TileIndex(-x_on_ring);
+                int n_far = coord2TileIndex(-y_on_ring);
+                // this is lerp, trust me...
+                global_min_arrival_times[m][n] = 0.5 * (global_min_arrival_times[m_close][n_close] + global_min_arrival_times[m_far][n_far]) +
+                                                 0.5 * (global_min_arrival_times[m_close][n_close] - global_min_arrival_times[m_far][n_far]) /
+                                                     radius_ratio;
+                reference_min_arrival_times[m][n] = global_min_arrival_times[m][n];
+            }
+        }
+}
+
+void interpolateTile(EventHeaderData *ed)
+{
+    float sectheta = 1 / cosf(ed->zenith);
+    std::array<short, 3> mnk;
+    std::array<short, 3> mnk_sample;
+    for (mnk[0] = NX / 2 - interp_halfside; mnk[0] < NX / 2 + interp_halfside; mnk[0] += 1)
+    {
+        for (mnk[1] = NY / 2 - interp_halfside; mnk[1] < NY / 2 + interp_halfside; mnk[1] += 1)
+        {
+            float x = tileIndex2Coord(mnk[0]);
+            float y = tileIndex2Coord(mnk[1]);
+            float r = hypotf(x, y);
+            if (r < interp_radius)
+            {
+                float zencor = hypotf(x / sectheta, y) / r;
+                float sampling_radius = interp_radius + 7.5; // offsetting to the next non-interpolated tile
+                float r_ratio = sampling_radius / r;
+                mnk_sample[0] = coord2TileIndex(100 * x * r_ratio);
+                mnk_sample[1] = coord2TileIndex(100 * y * r_ratio);
+
+                // e.g. if reference min arrival time happens to be 3*DT ahead of global min, this will be -3
+                short global2ref_delta_k = (short)((global_min_arrival_times[mnk_sample[0]][mnk_sample[1]] -
+                                                    reference_min_arrival_times[mnk_sample[0]][mnk_sample[1]]) /
+                                                   DT);
+
+                // for interpolated tiles reference min arr. time = global min arr. time, so k starts from 0
+                for (mnk[2] = 0; mnk[2] < TMAX; mnk[2] += 1)
+                {
+                    // interpolated k counts from 0 while sampled k -- from reference
+                    mnk_sample[2] = mnk[2] + global2ref_delta_k;
+
+                    unsigned short vem_top_sample;
+                    getValueOrZero(vem_top, mnk_sample, vem_top_sample);
+                    unsigned short vem_bot_sample;
+                    getValueOrZero(vem_bot, mnk_sample, vem_bot_sample);
+
+                    if (vem_top_sample == 0 && vem_bot_sample == 0)
+                        continue;
+
+                    unsigned short pz_sample;
+                    getValueOrZero(pz, mnk_sample, pz_sample);
+
+
+                    float vem_sample_to_interp_factor = powf(r_ratio, 2.6) * expf(zencor * (sampling_radius - r) / 575.0);
+
+                    float vem_top_interp = std::min((float)vem_top_sample * vem_sample_to_interp_factor, (float)60000.);
+                    vem_top[mnk] = (unsigned short)vem_top_interp;
+
+                    float vem_bot_interp = std::min((float)vem_bot_sample * vem_sample_to_interp_factor, (float)60000.);
+                    vem_bot[mnk] = (unsigned short)vem_bot_interp;
+
+                    float vem_mean_sample = 0.5 * ((float)vem_top_sample + (float)vem_bot_sample);
+                    float vem_mean_interp = 0.5 * (vem_top_interp + vem_bot_interp);
+                    float costheta_sample = (float)pz_sample / vem_mean_sample;
+                    // linear interpolation of cos theta between shower's (i.e. for particles near the core)
+                    // and sample tile's (for particles sampling_radius m from the core)
+                    float pz_interp = vem_mean_interp *
+                                      ((r / sampling_radius) * costheta_sample +
+                                       (1.0 - r / sampling_radius) * cosf(ed->zenith));
+                    pz[mnk] = (unsigned short)pz_interp;
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -148,6 +274,8 @@ int main(int argc, char *argv[])
     char *output_file = argv[2];
 
     EventHeaderData event_data;
+    initMinArrivalTimes(global_min_arrival_times);
+    initMinArrivalTimes(reference_min_arrival_times);
 
     FILE *flist = fopen(listing_file, "r");
 
@@ -159,98 +287,18 @@ int main(int argc, char *argv[])
     }
 
     char partial_tile_file[4096];
-    bool initial_file = true;
     while (fscanf(flist, "%s\n", partial_tile_file) != EOF)
     {
-        if (!loadPartialTileFile(partial_tile_file, &event_data, initial_file))
+        if (!loadPartialTileFile(partial_tile_file, &event_data))
             exit(EXIT_FAILURE);
-        initial_file = !initial_file;
-        fprintf(stdout, "Sparse matrices load (1%% = about 600Mb RAM): %f%%\n", 100 * sparse3DMatrixLoadFactor(vem_top));
+        fprintf(stdout, "Sparse matrices load (1%% ~ 600Mb RAM): %f%%\n", 100 * sparse3DMatrixLoadFactor(vem_top));
     }
+
+    inetrpolateMinArrivalTimes();
+    interpolateTile(&event_data);
 
     dumpTileFile(output_file, &event_data);
 
     fprintf(stdout, "OK\n");
     fclose(flist);
 }
-
-// void inetrpolateArrivalTimes()
-// {
-//     int m_edge = NX / 2;
-//     while (min_arrival_times[m_edge][NY / 2] == SENTINEL_TIME)
-//         m_edge++;
-//     interpolation_radius = tileIndex2Coord(m_edge) + 2.0;
-//     interpolation_tiles = m_edge - NX / 2 + 5;
-//     printf("Inner circle (interpolation area) radius: %g m\n", interpolation_radius);
-
-//     int m_closest, m_farthest, n_closest, n_farthest;
-//     float x, y, x_ring_closest, y_ring_closest;
-//     float rad_fraction;
-//     for (int m = NX / 2 - interpolation_tiles; m < NX / 2 + interpolation_tiles; m++)
-//         for (int n = NY / 2 - interpolation_tiles; n < NY / 2 + interpolation_tiles; n++)
-//         {
-//             x = tileIndex2Coord(m);
-//             y = tileIndex2Coord(n);
-//             if (hypotf(x, y) < interpolation_radius)
-//             {
-//                 rad_fraction = (interpolation_radius + 7.5) / hypotf(x, y);
-//                 x_ring_closest = 100 * x * rad_fraction; // m -> cm
-//                 y_ring_closest = 100 * y * rad_fraction; // m -> cm
-//                 m_closest = coord2TileIndex(x_ring_closest);
-//                 n_closest = coord2TileIndex(y_ring_closest);
-//                 m_farthest = coord2TileIndex(-x_ring_closest);
-//                 n_farthest = coord2TileIndex(-y_ring_closest);
-//                 min_arrival_times[m][n] = 0.5 * (min_arrival_times[m_closest][n_closest] + min_arrival_times[m_farthest][n_farthest]);
-//                 min_arrival_times[m][n] += (min_arrival_times[m_closest][n_closest] - min_arrival_times[m_farthest][n_farthest]) /
-//                                            rad_fraction /
-//                                            2.0;
-//             }
-//         }
-// }
-
-// void interpolateVemCounts(EventHeaderData *ed)
-// {
-//     float sectheta = 1 / cosf(ed->zenith);
-//     for (int m = NX / 2 - interpolation_tiles; m < NX / 2 + interpolation_tiles; m++)
-//     {
-//         for (int n = NY / 2 - interpolation_tiles; n < NY / 2 + interpolation_tiles; n++)
-//         {
-//             float x = tileIndex2Coord(m);
-//             float y = tileIndex2Coord(n);
-//             float radius = hypotf(x, y);
-//             float zencor = hypotf(x / sectheta, y) / radius;
-//             if (radius < interpolation_radius)
-//             {
-//                 float sampling_radius = interpolation_radius + 7.5; // offsetting to the next non-interpolated tile
-//                 float radius_frac = sampling_radius / radius;
-//                 int m_sample = coord2TileIndex(100 * x * radius_frac);
-//                 int n_sample = coord2TileIndex(100 * y * radius_frac);
-//                 for (int k = 0; k < NT; k++)
-//                 {
-//                     for (int l = 0; l < 2; l++)
-//                     {
-//                         float vem_tmp =
-//                             (float)vemcount[m_sample][n_sample][k][l] *
-//                             powf(radius_frac, 2.6) *
-//                             expf(zencor * (sampling_radius - radius) / 575.0);
-//                         if (vem_tmp > 60000.)
-//                             vem_tmp = 60000.;
-//                         vemcount[m][n][k][l] = (unsigned short)vem_tmp;
-//                     }
-
-//                     float vemcount_sample_mean = 0.5 * ((float)vemcount[m_sample][n_sample][k][0] +
-//                                                         (float)vemcount[m_sample][n_sample][k][1]);
-//                     float vemcount_mean = 0.5 * ((float)vemcount[m][n][k][0] +
-//                                                  (float)vemcount[m][n][k][1]);
-//                     float costheta_sample = (float)pz[m_sample][n_sample][k] / vemcount_sample_mean;
-//                     // linear interpolation of cos theta between shower's (= particles near the core)
-//                     // and sample tile's (particles sampling_radius m from the core)
-//                     float pz_ = vemcount_mean *
-//                                 ((radius / sampling_radius) * costheta_sample +
-//                                  (1.0 - radius / sampling_radius) * cosf(ed->zenith));
-//                     pz[m][n][k] = (unsigned short)pz_;
-//                 }
-//             }
-//         }
-//     }
-// }
