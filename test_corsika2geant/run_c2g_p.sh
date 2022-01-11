4d9aaaa368bf6fd26e3ef701039a59eeea8a4c84
Igor Vaiman <gosha.vaiman@gmail.com>
2022-01-11 18:09:13 +0300

    testing dir removed from repo

--- test_corsika2geant/run_c2g_p.sh
+++
@@ -1,5 +0,0 @@
-for DETHIN in *.dethinned
-do
-    corsika2geant_parallel_process.run $DETHIN ../data/sdgeant.dst $DETHIN.tile
-    # break;
-done
--- test_corsika2geant/run_dethinning.sh
+++
@@ -1,4 +0,0 @@
-for file in DAT??????.p*
-do
-    dethinning.run $file $file.dethinned
-done
