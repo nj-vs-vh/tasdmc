a8e0b8191f9b14db1e96000ac34b58c38107e767
Igor Vaiman <gosha.vaiman@gmail.com>
2022-01-27 11:34:24 +0300

    new Bank object to dispatch calls to custom accessors

--- src/utils/dstreader/examples/waveforms.py
+++ src/utils/dstreader/examples/waveforms.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from dstreader import DstFile
-from dstreader.dstreader_core import get_rusdraw_xxyy, get_rusdraw_fadc
 
 
 # example_dst_file = Path(__file__).parent / 'example.dst.gz'
@@ -11,10 +10,9 @@ with DstFile(example_dst_file) as dst:
         if 'rusdraw' not in banks:
             print("rusdmc bank not found!")
         rusdraw = dst.get_bank('rusdraw')
-        if rusdraw.nofwf == 0:
+        if rusdraw['nofwf'] == 0:
             continue
-        xxyy = get_rusdraw_xxyy()
-        print(xxyy)  # regular numpy array
-        fadc = get_rusdraw_fadc()
+        print(rusdraw['xxyy'])  # regular numpy array
+        fadc = rusdraw['fadc']
         print(fadc[:, 0, :])  # printing waveforms for top counter
         break
---
+++ src/utils/dstreader/src/bank.py
@@ -0,0 +1,62 @@
+import warnings
+
+from typing import TypeVar, Union
+from numbers import Number
+from numpy.typing import NDArray
+
+from . import dstreader_core as dstc
+
+
+_SwigGeneratedBankObject = TypeVar("_SwigGeneratedBankObject")
+
+_SwigOpaquePointer = TypeVar("_SwigOpaquePointer")
+
+
+class Bank:
+    """Generic wrapper for bank object, its only job is to dispatch user to custom accessors
+    whenever they request an array field. The preferred way to obtain an instance of Bank is
+    via DstFile's get_bank method.
+
+    To get a field from bank, use dict-like syntax:
+    >>> rusdraw = dst.get_bak("rusdraw")
+    >>> fadc = rusdraw["fadc"]
+    """
+
+    def __init__(self, name: str, bank_obj: _SwigGeneratedBankObject):
+        self.name = name  # name is stored without trailing underscore
+        self.bank_obj = bank_obj
+        self.bank_class = bank_obj.__class__
+
+    def keys(self):
+        return [
+            p
+            for p in dir(self.bank_class)
+            # thisown property is internal SWIG's stuff
+            if p != "thisown" and isinstance(getattr(self.bank_class, p), property)
+        ]
+
+    def __getitem__(self, key: str) -> Union[Number, NDArray, _SwigOpaquePointer]:
+        value = getattr(self.bank_obj, key, None)
+        if value is None:
+            raise KeyError(
+                f"{self.name} bank does not containt {key} field! Use .keys() method to see available fields"
+            )
+        if type(value).__name__ == "SwigPyObject":
+            # ooops seems like an opaque SWIG object, probably an array, maybe we have a custom accessor for it?
+            accessor_func_name = f"get_{self.name}_{key}"
+            accessor_func = getattr(dstc, accessor_func_name, None)
+            if accessor_func is None:
+                warnings.warn(
+                    f"Can't interpret value of field {key} in a meaningful way: no default nor "
+                    + "custom accessors seem to exist; returning opaque pointer, but you "
+                    + "will likely not be able to use it.",
+                    RuntimeWarning,
+                )
+                value: _SwigOpaquePointer
+                return value
+            else:
+                value: NDArray = accessor_func()
+                return value
+        else:
+            value: Number
+            return value
