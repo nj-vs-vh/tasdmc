4a0e3b73b9a4003572c6c1d087f31022174a5619
Igor Vaiman <gosha.vaiman@gmail.com>
2022-02-17 15:10:46 +0300

    better defaults for inspect cmd

--- src/tasdmc/cli/commands/inspection.py
+++ src/tasdmc/cli/commands/inspection.py
@@ -26,13 +26,13 @@ def fix_failed_pipelines_cmd(hard: bool):
 
 @cli.command("inspect", help="Step-by-step inspection of pipelines in RUN_NAME")
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Print verbose information about steps")
-@click.option("-p", "--page", "pagesize", default=0, help="Page size; 0 for no pagination (default)")
-@click.option("-f", "--failed", is_flag=True, default=False, help="Inspect only failed pipelines")
+@click.option("-p", "--page", "pagesize", default=1, help="Page size; 0 for no pagination")
+@click.option("-a", "--all", is_flag=True, default=False, help="Inspect all pipelines, not only failed ones")
 @loading_run_by_name
 @error_catching
-def inspect_cmd(pagesize: int, verbose: bool, failed: bool):
+def inspect_cmd(pagesize: int, verbose: bool, all: bool):
     if config.is_distributed_run():
         click.echo("Not available for distributed run, please inspect your nodes manually")
         return
-    pipeline_ids = fileio.get_failed_pipeline_ids() if failed else fileio.get_all_pipeline_ids()
+    pipeline_ids = fileio.get_all_pipeline_ids() if all else fileio.get_failed_pipeline_ids()
     inspect.inspect_pipelines(pipeline_ids, page_size=pagesize, verbose=verbose, fix=False)
